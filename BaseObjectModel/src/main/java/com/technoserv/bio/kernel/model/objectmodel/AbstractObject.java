0ffa3b969ad77694807fd4003c5c117d6eb270b1
andrey <andreymav@gmail.com>
2016-11-15 22:23:57 +0300

    Id

--- BaseObjectModel/src/main/java/com/technoserv/bio/kernel/model/objectmodel/AbstractObject.java
+++ BaseObjectModel/src/main/java/com/technoserv/bio/kernel/model/objectmodel/AbstractObject.java
@@ -2,31 +2,35 @@ package com.technoserv.bio.kernel.model.objectmodel;
 
 import com.technoserv.bio.kernel.model.BaseEntity;
 
+import javax.persistence.*;
 import java.util.Date;
 
+@MappedSuperclass
 public abstract class AbstractObject extends BaseEntity<Long> {
-	/**
-	 *  уникальный идентификатор объекта
-	 */
-	private Long id;
-	/**
-	 *  дата создания объекта
-	 */
-	private Date objectDate;
+    /**
+     * уникальный идентификатор объекта
+     */
+    @Id
+    @GeneratedValue
+    private Long id;
+    /**
+     * дата создания объекта
+     */
+    @Column
+    @Temporal(value = TemporalType.DATE)
+    private Date objectDate;
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Date getObjectDate() {
-		return objectDate;
-	}
-
-	public void setObjectDate(Date objectDate) {
-		this.objectDate = objectDate;
-	}
+    public Date getObjectDate() {
+        return objectDate;
+    }
+    public void setObjectDate(Date objectDate) {
+        this.objectDate = objectDate;
+    }
 }
