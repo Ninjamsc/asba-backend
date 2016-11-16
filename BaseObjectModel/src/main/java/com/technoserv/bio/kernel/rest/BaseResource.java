412e2ae4e155f826a1fb02d3a2867f46d85d94ed
Andrey <andrey.malushko@surasystems.com>
2016-11-16 10:37:37 +0300

    resource

--- BaseObjectModel/src/main/java/com/technoserv/bio/kernel/rest/BaseResource.java
+++ BaseObjectModel/src/main/java/com/technoserv/bio/kernel/rest/BaseResource.java
@@ -1,13 +1,8 @@
 package com.technoserv.bio.kernel.rest;
 
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.jaxrs.annotation.JacksonFeatures;
 import com.technoserv.bio.kernel.model.BaseEntity;
 import com.technoserv.bio.kernel.service.Service;
-import io.swagger.annotations.Api;
 
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.Serializable;
 import java.util.Collection;
@@ -16,44 +11,24 @@ public abstract class BaseResource<K extends Serializable, T extends BaseEntity<
 
     protected abstract Service<K, T> getBaseService();
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    @JacksonFeatures( serializationEnable =  { SerializationFeature.INDENT_OUTPUT })
     public Collection<T> list() {
          return getBaseService().getAll();
     }
 
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    @JacksonFeatures( serializationEnable =  { SerializationFeature.INDENT_OUTPUT })
+    public T get(K id) {
+        return getBaseService().findById(id);
+    }
+
     public K add(T entity) {
         return getBaseService().save(entity);
     }
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    @JacksonFeatures( serializationEnable =  { SerializationFeature.INDENT_OUTPUT })
-    public T get(@QueryParam("id") K id) {
-        return getBaseService().findById(id);
-    }
-
-    @PUT
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    @JacksonFeatures(serializationEnable =  { SerializationFeature.INDENT_OUTPUT })
     public Response update(T entity) {
         getBaseService().saveOrUpdate(entity);
         return Response.ok().build();
     }
 
-    @DELETE
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    @JacksonFeatures(serializationEnable =  { SerializationFeature.INDENT_OUTPUT })
-    public Response delete(@QueryParam("id") K id) {
+    public Response delete(K id) {
         getBaseService().delete(id);
         return Response.ok().build();
     }
--- BaseObjectModel/src/main/java/com/technoserv/bio/kernel/rest/FrontEndConfigurationResource.java
+++ BaseObjectModel/src/main/java/com/technoserv/bio/kernel/rest/FrontEndConfigurationResource.java
@@ -1,6 +1,8 @@
 package com.technoserv.bio.kernel.rest;
 
 
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.jaxrs.annotation.JacksonFeatures;
 import com.technoserv.bio.kernel.model.configuration.FrontEndConfiguration;
 import com.technoserv.bio.kernel.service.Service;
 import com.technoserv.bio.kernel.service.configuration.api.FrontEndConfigurationService;
@@ -9,6 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Collection;
 
 @Component
 @Path("/v1/front-end-configuration")
@@ -21,5 +26,48 @@ public class FrontEndConfigurationResource extends BaseResource<Long, FrontEndCo
     @Override
     protected Service<Long, FrontEndConfiguration> getBaseService() {
         return service;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @JacksonFeatures( serializationEnable =  { SerializationFeature.INDENT_OUTPUT })
+    @Path("list")
+    public Collection<FrontEndConfiguration> list() {
+        return getBaseService().getAll();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @JacksonFeatures( serializationEnable =  { SerializationFeature.INDENT_OUTPUT })
+    public FrontEndConfiguration get(@QueryParam("id") Long id) {
+        return getBaseService().findById(id);
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @JacksonFeatures( serializationEnable =  { SerializationFeature.INDENT_OUTPUT })
+    public Long add(FrontEndConfiguration entity) {
+        return getBaseService().save(entity);
+    }
+
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @JacksonFeatures(serializationEnable =  { SerializationFeature.INDENT_OUTPUT })
+    public Response update(FrontEndConfiguration entity) {
+        getBaseService().saveOrUpdate(entity);
+        return Response.ok().build();
+    }
+
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @JacksonFeatures(serializationEnable =  { SerializationFeature.INDENT_OUTPUT })
+    public Response delete(@QueryParam("id") Long id) {
+        getBaseService().delete(id);
+        return Response.ok().build();
     }
 }
