89e28bb7f18199c78c3fb0ab4e082cc3ca5fe958
Dorohov <dorohov@lanit.ru>
2016-11-20 16:41:06 +0300

    rest put

--- amqp/src/main/java/com/technoserv/jms/HttpRestClient.java
+++ amqp/src/main/java/com/technoserv/jms/HttpRestClient.java
@@ -1,6 +1,7 @@
 package com.technoserv.jms;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
@@ -11,11 +12,20 @@ import java.net.URI;
 @Service
 public class HttpRestClient {
 
-    public static final String URL = "";
+    public static final String URL = "http://www.google.ru/";
+    private RestTemplate rest = new RestTemplate();
 
     public void put(String message) {
         System.out.println("SEND");
-        RestTemplate rest = new RestTemplate();
-        rest.put(URI.create(URL), message);
+
+        try {
+            rest.put(URI.create(URL), message);
+
+        }catch (RestClientException e) {
+
+            //todo сделать обработку исключений
+            e.printStackTrace();
+
+        }
     }
 }
