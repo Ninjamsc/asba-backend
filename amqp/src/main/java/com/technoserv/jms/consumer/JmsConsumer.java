3c686651eb549d26deedc6302a6fdb4b6c45f916
Dorohov <dorohov@lanit.ru>
2016-11-20 19:06:28 +0300

    rest put

--- amqp/src/main/java/com/technoserv/jms/consumer/JmsConsumer.java
+++ amqp/src/main/java/com/technoserv/jms/consumer/JmsConsumer.java
@@ -3,16 +3,20 @@ package com.technoserv.jms.consumer;
 import com.technoserv.jms.HttpRestClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
-@Component
 public class JmsConsumer {
 
     @Autowired
     private HttpRestClient httpRestClient;
 
-    @JmsListener(destination = "internal.queue")
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
     public void onReceive(String message) {
-        httpRestClient.put(message);
+        if(!httpRestClient.put(message)) {
+            jmsTemplate.convertAndSend(message);
+        }
     }
 }
--- amqp/src/main/java/com/technoserv/jms/producer/JmsProducer.java
+++ amqp/src/main/java/com/technoserv/jms/producer/JmsProducer.java
@@ -1,6 +1,7 @@
 package com.technoserv.jms.producer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ public class JmsProducer {
     private JmsTemplate jmsTemplate;
 
     @Autowired
+    @Qualifier("internal.queue")
     private Queue queue;
 
     public void send(String msg) {
