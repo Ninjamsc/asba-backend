8357f82c59561a195a9f3b0e0df481455bce980d
andrey <andreymav@gmail.com>
2016-11-20 22:15:24 +0300

    IT test

--- amqp/src/test/java/com/technoserv/jms/client/impl/JmsClientTest.java
+++ amqp/src/test/java/com/technoserv/jms/client/impl/JmsClientTest.java
@@ -1,7 +1,14 @@
 package com.technoserv.jms.client.impl;
 
+import com.technoserv.jms.trusted.RetryMessage;
+import org.apache.activemq.broker.BrokerService;
 import org.apache.activemq.spring.ActiveMQConnectionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.jms.ConnectionFactory;
 import java.util.Arrays;
@@ -9,13 +16,25 @@ import java.util.Arrays;
 /**
 * Created by Adrey on 20.11.2016.
 */
-
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/testContext.xml")
 public class JmsClientTest {
+
+    @Autowired
+    private BrokerService brokerService;
+
     public static void main(String[] args) {
         JmsTemplate template = new JmsTemplate();
         template.setConnectionFactory(connectionFactory());
         template.convertAndSend("external.queue","Test message");
+    }
+
+    @Test
+    public void internalIT() throws Exception {
+        JmsTemplate template = new JmsTemplate();
+        template.setConnectionFactory(internalConnectionFactory());
+        template.convertAndSend("internal.queue", new RetryMessage("Test message"));
+
     }
 
     public static ConnectionFactory connectionFactory(){
@@ -24,6 +43,9 @@ public class JmsClientTest {
         return connectionFactory;
     }
 
-
+    public static ConnectionFactory internalConnectionFactory(){
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+        connectionFactory.setBrokerURL("vm://localhost");
+        return connectionFactory;
+    }
 }
-
