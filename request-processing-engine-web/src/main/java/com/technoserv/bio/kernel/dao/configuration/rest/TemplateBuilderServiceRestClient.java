7e8f4783173bd81f251ad83060433c9359824de5
Vadim Basakov <vbasakov@gmail.com>
2016-11-23 01:23:47 +0400

    template service properties

--- request-processing-engine-web/src/main/java/com/technoserv/bio/kernel/dao/configuration/rest/TemplateBuilderServiceRestClient.java
+++ request-processing-engine-web/src/main/java/com/technoserv/bio/kernel/dao/configuration/rest/TemplateBuilderServiceRestClient.java
@@ -20,12 +20,12 @@ import java.net.URI;
  * Created by VBasakov on 22.11.2016.
  */
 @Service
-@PropertySource("classpath:TemplateBuilderServiceRestClient.properties")
+@PropertySource("classpath:templateBuilderServiceRestClient.properties")
 public class TemplateBuilderServiceRestClient {
 
     private static final Log log = LogFactory.getLog(TemplateBuilderServiceRestClient.class);
 
-    @Value("${http.rest.client.url}")
+    @Value("${http.template.builder.service.client.url}")
     private String url;
 
     private RestTemplate rest = new RestTemplate();
