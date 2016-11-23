cca957a128260e9fc33c216d07c9e113ea7077c3
Andrey <andreymav@gmail.com>
2016-11-24 00:13:26 +0300

    override

--- common/src/main/java/com/technoserv/db/dao/objectmodel/impl/PersonDaoImpl.java
+++ common/src/main/java/com/technoserv/db/dao/objectmodel/impl/PersonDaoImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Repository;
  */
 @Repository("personDao")
 public class PersonDaoImpl extends AbstractHibernateDao<Long,Person> implements PersonDao {
-    @Override
+
     public Person history(Long iin) {
         Criteria criteria = getSession().createCriteria(getPersistentClass()).add(Property.forName("id").eq(iin));
         criteria.createCriteria("dossier","d", JoinType.INNER_JOIN);
