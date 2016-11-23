515fc92a4bde19d58178068aa801a2a949025440
Dorohov <dorohov@lanit.ru>
2016-11-23 13:32:12 +0300

    Новая модель данных

--- common/src/main/java/com/technoserv/db/model/objectmodel/BioTemplate.java
+++ common/src/main/java/com/technoserv/db/model/objectmodel/BioTemplate.java
@@ -1,9 +1,6 @@
 package com.technoserv.db.model.objectmodel;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 /**
  * Хранилище биометрических шаблонов (векторов).
@@ -18,11 +15,57 @@ public class BioTemplate extends AbstractObject {
     @JoinColumn(name = "DOC_ID", referencedColumnName = "ID")
     private Document document;
 
+    @ManyToOne
+    @JoinColumn(name = "BTMPLT_ID", referencedColumnName = "ID")
+    private BioTemplateType bioTemplateType;
+
+    @ManyToOne
+    @JoinColumn(name = "BTMPLV_ID", referencedColumnName = "ID")
+    private BioTemplateVersion bioTemplateVersion;
+
+    @Column(name = "INS_USER")
+    private String insUser;
+
+    @Column(name = "TEMPLATE_VECTOR")
+    private String templateVector;
+
     public Document getDocument() {
         return document;
     }
 
     public void setDocument(Document document) {
         this.document = document;
+    }
+
+    public BioTemplateType getBioTemplateType() {
+        return bioTemplateType;
+    }
+
+    public void setBioTemplateType(BioTemplateType bioTemplateType) {
+        this.bioTemplateType = bioTemplateType;
+    }
+
+    public BioTemplateVersion getBioTemplateVersion() {
+        return bioTemplateVersion;
+    }
+
+    public void setBioTemplateVersion(BioTemplateVersion bioTemplateVersion) {
+        this.bioTemplateVersion = bioTemplateVersion;
+    }
+
+    public String getInsUser() {
+        return insUser;
+    }
+
+    public void setInsUser(String insUser) {
+        this.insUser = insUser;
+    }
+
+    public String getTemplateVector() {
+        return templateVector;
+    }
+
+    public void setTemplateVector(String templateVector) {
+        this.templateVector = templateVector;
     }
 }
--- common/src/main/java/com/technoserv/db/model/objectmodel/BioTemplateType.java
+++ common/src/main/java/com/technoserv/db/model/objectmodel/BioTemplateType.java
@@ -1,10 +1,14 @@
 package com.technoserv.db.model.objectmodel;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 /**
  * Справочник типов биометрических шаблонов (Фото, вены, отпечатки пальцев и т.п.)
  */
+@Entity
+@Table(name="BIO_TEMPLATES_TYPES")
 public class BioTemplateType extends AbstractObject {
 
     @Column(name = "DESCRIPTION", nullable = false)
