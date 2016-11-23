59efb10c7182b6551670dc90f7db58beaa2f6edf
Dorohov <dorohov@lanit.ru>
2016-11-23 22:35:49 +0300

    Рест со стоп листами

--- common/src/main/java/com/technoserv/db/dao/objectmodel/impl/RequestDaoImpl.java
+++ common/src/main/java/com/technoserv/db/dao/objectmodel/impl/RequestDaoImpl.java
@@ -3,23 +3,57 @@ package com.technoserv.db.dao.objectmodel.impl;
 import com.technoserv.db.dao.AbstractHibernateDao;
 import com.technoserv.db.dao.objectmodel.api.RequestDao;
 import com.technoserv.db.model.objectmodel.Request;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Conjunction;
+import org.hibernate.criterion.Disjunction;
 import org.hibernate.criterion.Property;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
+import java.util.Calendar;
 import java.util.Collection;
+import java.util.Date;
 
 /**
  * Created by sergey on 15.11.2016.
  */
 @Repository("requestDao")
 public class RequestDaoImpl extends AbstractHibernateDao<Long, Request> implements RequestDao {
+
+    private static final int AMOUNT_TTL = -15;//Колличество прибавляемых единиц
+    private static final int FIELD_TTL = Calendar.MINUTE;//Единицап измерения времени
+
     public Request findByOrderNumber(Long id) {
         return (Request) getSession().createCriteria(getPersistentClass())
                 .add(Property.forName("wfmID").eq(id)).uniqueResult();
     }
 
+    /**
+     * Находим запросы где заполнены все изображения или где прошло более N минут
+     * @return
+     */
     public Collection<Request> findNotProcessed() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.add(FIELD_TTL, AMOUNT_TTL);
+        Date ttlDate = calendar.getTime();
+        Criteria criteria = getSession().createCriteria(getPersistentClass());
+        criteria.createCriteria("scannedDocument","sd");
+        criteria.createCriteria("cameraDocument","cd");
+        
+        Disjunction disjunction = Restrictions.disjunction();
+
+        Conjunction conjunction = Restrictions.conjunction();
+        conjunction.add(Property.forName("sd.origImageURL").isNotNull());
+        conjunction.add(Property.forName("sd.faceSquare").isNotNull());
+        conjunction.add(Property.forName("cd.origImageURL").isNotNull());
+        conjunction.add(Property.forName("cd.faceSquare").isNotNull());
+        disjunction.add(conjunction);
+        disjunction.add(Property.forName("objectDate").le(ttlDate));
+        criteria.add(disjunction);
+
+        criteria.add(Property.forName("status").eq(Request.Status.SAVED));
         return getSession().createCriteria(getPersistentClass())
-                .add(Property.forName("status").eq(Request.Status.SAVED)).list();
+                .add(Property.forName("status").eq(Request.Status.SAVED)).add(Property.forName("objectDate").ge(calendar.getTime())).list();
     }
 }
