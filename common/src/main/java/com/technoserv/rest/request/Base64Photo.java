62e724782d67bdead98e6667c60083485869e230
Dorohov <dorohov@lanit.ru>
2016-11-25 18:47:44 +0300

    Переписал рест клиент

--- common/src/main/java/com/technoserv/rest/request/Base64Photo.java
+++ common/src/main/java/com/technoserv/rest/request/Base64Photo.java
@@ -1,6 +1,10 @@
 package com.technoserv.rest.request;
 
-import java.util.Base64;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.apache.commons.codec.binary.Base64;
+
 /**
  * Фото в формате base64.
  * Структура класса соответствует структуре json-запроса к большинству сервисов *
@@ -11,7 +15,26 @@ public class Base64Photo {
     /** base46 encoded изображение */
     public String photo;
 
-    public Base64Photo(byte[] base64photo) {
-        photo = Base64.getEncoder().encodeToString(base64photo);
+    public Base64Photo(byte[] imageByteArray) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("data:image/jpg;base64,");
+        sb.append(new String(Base64.encodeBase64(imageByteArray)));
+        photo = sb.toString();
     }
+
+//    public static void main(String[] args) throws IOException {
+//        FileInputStream fileInputStream=null;
+//
+//        File file = new File("D:\\images\\01-spitzer-omeganebula.jpg");
+//
+//        byte[] bFile = new byte[(int) file.length()];
+//
+//            fileInputStream = new FileInputStream(file);
+//            fileInputStream.read(bFile);
+//            fileInputStream.close();
+//
+//
+//            System.out.println(new Base64Photo(bFile).photo);
+//    }
+
 }
