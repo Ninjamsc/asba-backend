c70e6b43dcec27fa39026fabb804684fc0703e5c
Vadim Basakov <vbasakov@gmail.com>
2016-11-28 00:39:31 +0400

    StopLists quick workaround

--- common/src/main/java/com/technoserv/db/dao/objectmodel/impl/StopListDaoImpl.java
+++ common/src/main/java/com/technoserv/db/dao/objectmodel/impl/StopListDaoImpl.java
@@ -3,11 +3,21 @@ package com.technoserv.db.dao.objectmodel.impl;
 import com.technoserv.db.dao.AbstractHibernateDao;
 import com.technoserv.db.dao.objectmodel.api.StopListDao;
 import com.technoserv.db.model.objectmodel.StopList;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.CriteriaSpecification;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by sergey on 15.11.2016.
  */
 @Repository("stopListDao")
 public class StopListDaoImpl extends AbstractHibernateDao<Long,StopList> implements StopListDao {
+    @Override
+    public List<StopList> getAll() {
+        Criteria criteria = getSession().createCriteria(getPersistentClass());
+        criteria.setResultTransformer(CriteriaSpecification.DISTINCT_ROOT_ENTITY);
+        return criteria.list();
+    }
 }
