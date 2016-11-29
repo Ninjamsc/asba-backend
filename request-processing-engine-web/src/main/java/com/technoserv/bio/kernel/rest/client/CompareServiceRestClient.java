e5ad59adf4d2a4d750e1d65a7d0d915d296fbfa7
Dorohov <dorohov@lanit.ru>
2016-11-29 20:31:39 +0300

    Доработка RequestProcessor-а

--- request-processing-engine-web/src/main/java/com/technoserv/bio/kernel/rest/client/CompareServiceRestClient.java
+++ request-processing-engine-web/src/main/java/com/technoserv/bio/kernel/rest/client/CompareServiceRestClient.java
@@ -44,12 +44,13 @@ public class CompareServiceRestClient {
             return response.getBody();
         } catch (RestClientResponseException e) {
             log.error("COMPARING TEMPLATE ERROR CODE " + e.getRawStatusCode());
-            switch (e.getRawStatusCode()){
+//            switch (e.getRawStatusCode()){
                 /*Стандартные названия ошибок не совпадают с нашей документацией только коды */
-                case 400:log.error("BAD_REQUEST");throw new CompareServiceException(e.getResponseBodyAsString());//BAD_REQUEST:
-                case 500:log.error("INTERNAL_SERVER_ERROR");throw new CompareServiceException(e.getResponseBodyAsString());//INTERNAL_SERVER_ERROR:
-                default: throw new RuntimeException(e.getResponseBodyAsString());
-            }
+//                case 400:log.error("BAD_REQUEST");throw new CompareServiceException(e.getResponseBodyAsString());//BAD_REQUEST:
+//                case 500:log.error("INTERNAL_SERVER_ERROR");throw new CompareServiceException(e.getResponseBodyAsString());//INTERNAL_SERVER_ERROR:
+//                default: throw new RuntimeException(e.getResponseBodyAsString());
+//            }
+            throw new RuntimeException(e);
         }
     }
 }
