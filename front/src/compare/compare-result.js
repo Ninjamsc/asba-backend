0002609eaf85a85947e9d565cababca352710208
Vadim Basakov <vbasakov@gmail.com>
2016-12-14 23:11:02 +0500

    подгонка под рельный json

--- front/src/compare/compare-result.js
+++ front/src/compare/compare-result.js
@@ -6,19 +6,8 @@ angular.module('compare-result-view', ['ui.router', 'commons'])
             $log.info($scope.creditRequestIdForSearch);
             $httpService.findCompareResult($scope.creditRequestIdForSearch, function (result) {
                 console.log(result.data);
-                var data = result.data;
-                var res = {};
 
-                res.rules = data.rules;
-                res.scannedPicture = {
-                    pictureURL: data.scannedPicture.pictureURL,
-                    previewURL: data.scannedPicture.previewURL
-                };
-                res.cameraPicture = {
-                    pictureURL: data.cameraPicture.pictureURL,
-                    previewURL: data.cameraPicture.previewURL
-                };
-                $scope.compareResult = res;
+                $scope.compareResult = result.data;
             });
         };
 
