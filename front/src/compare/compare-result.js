180dba523bdc2923f6424bf2e2a61a3d16c852ce
Vadim Basakov <vbasakov@gmail.com>
2016-12-14 21:37:06 +0500

    подгонка под рельный json

--- front/src/compare/compare-result.js
+++ front/src/compare/compare-result.js
@@ -1,15 +1,26 @@
 angular.module('compare-result-view', ['ui.router', 'commons'])
-    .controller('compareResultViewController', function ($scope, $log, $state, $stateParams,$httpService) {
+    .controller('compareResultViewController', function ($scope, $log, $state, $stateParams, $httpService) {
 
 
-
-        $scope.findRequest = function(){
+        $scope.findRequest = function () {
             $log.info($scope.creditRequestIdForSearch);
             $httpService.findCompareResult($scope.creditRequestIdForSearch, function (result) {
-                $scope.compareResult = result.data;
+                console.log(result.data);
+                var data = result.data;
+                var res = {};
+
+                res.rules = data.rules;
+                res.scannedPicture = {
+                    pictureURL: data.scannedPicture.pictureURL,
+                    previewURL: data.scannedPicture.previewURL
+                };
+                res.cameraPicture = {
+                    pictureURL: data.cameraPicture.pictureURL,
+                    previewURL: data.cameraPicture.previewURL
+                };
+                $scope.compareResult = res;
             });
         };
-
 
 
         //todo remove
