dcf085e3e1bcae072ee2fe6e0f6506596078a58b
Vadim Basakov <vbasakov@gmail.com>
2016-12-15 02:07:51 +0500

    bug fix

--- front/src/compare/compare-result.js
+++ front/src/compare/compare-result.js
@@ -1,13 +1,6 @@
 angular.module('compare-result-view', ['ui.router', 'commons'])
     .controller('compareResultViewController', function ($scope, $log, $state, $stateParams, $httpService) {
 
-
-        $scope.onSearchTextFieldKeydown = function ($event) {
-            if ($event.which === 13 || $event.which === 32) {
-                findRequest();
-            }
-        };
-
         $scope.findRequest = function () {
             $log.info($scope.creditRequestIdForSearch);
             $httpService.findCompareResult($scope.creditRequestIdForSearch, function (result) {
@@ -28,16 +21,22 @@ angular.module('compare-result-view', ['ui.router', 'commons'])
                 result.scannedPicture.pictureURL = data.scannedPictureURL;
                 result.scannedPicture.previewURL = data.scannedPicturePreviewURL;
                 /*"wfNumber" : 11154,
-                    "IIN" : 11154,
-                    "username" : "OperatorName",
-                    "timestamp" : "12.12.16.12.00.12",
-                    "scannedPictureURL" : "http://www.sdorohov.ru/storage/rest/image/MzlmYzM2YTEtNDdlMy00NjIzLThiMTYtNGZmNTg4M2RlYmVi.jpg",
-                    "scannedPicturePreviewURL" : null,
-                    "webCamPictureURL" : "http://www.sdorohov.ru/storage/rest/image/YWNjMDdlZmEtMTJjMS00MTM3LWIwNGYtOTBmMWIzMjIyZDdm.jpg",
-                    "webCamPicturePreviewURL" : null*/
+                 "IIN" : 11154,
+                 "username" : "OperatorName",
+                 "timestamp" : "12.12.16.12.00.12",
+                 "scannedPictureURL" : "http://www.sdorohov.ru/storage/rest/image/MzlmYzM2YTEtNDdlMy00NjIzLThiMTYtNGZmNTg4M2RlYmVi.jpg",
+                 "scannedPicturePreviewURL" : null,
+                 "webCamPictureURL" : "http://www.sdorohov.ru/storage/rest/image/YWNjMDdlZmEtMTJjMS00MTM3LWIwNGYtOTBmMWIzMjIyZDdm.jpg",
+                 "webCamPicturePreviewURL" : null*/
                 $scope.compareResult = result;
 
             });
+        };
+
+        $scope.onSearchTextFieldKeydown = function ($event) {
+            if ($event.which === 13 || $event.which === 32) {
+                $scope.findRequest();
+            }
         };
 
 
