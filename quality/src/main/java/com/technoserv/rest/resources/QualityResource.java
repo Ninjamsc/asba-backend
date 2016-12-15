00f7a7bf69ac61366f3bb27fd6fe166c50e00df3
Bitbucket's admin <mlevitin@technoserv.com>
2016-12-15 14:53:12 +0300

    quality service

---
+++ quality/src/main/java/com/technoserv/rest/resources/QualityResource.java
@@ -0,0 +1,27 @@
+package com.technoserv.rest.resources;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import io.swagger.annotations.Api;
+import org.springframework.stereotype.Component;
+
+@Component
+@Path("")
+@Api(value = "quality")
+public class QualityResource {
+
+
+	@PUT
+	@Path("/test")
+	//@Consumes(MediaType.APPLICATION_XML)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response updateMessage(QualityRequest message) {
+		return Response.status(200).build();
+	}
+
+
+}
