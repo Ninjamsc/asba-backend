4855842474b0efff9b3b9cf96b9bb00ff125c3f7
Bitbucket's admin <mlevitin@technoserv.com>
2016-12-15 15:42:53 +0300

    quality service

---
+++ quality/src/main/java/com/technoserv/rest/resources/QualityRequest.java
@@ -0,0 +1,21 @@
+
+package com.technoserv.rest.resources;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class QualityRequest {
+private String photos;
+
+public String getPhotos() {
+	return photos;
+}
+
+public void setPhotos(String photos) {
+	this.photos = photos;
+}
+
+@Override
+public String toString() {
+	return "QualityRequest  [photos=" + photos + "]";
+}
+}
