e4daa78a9cef1fb55b23de16fd2e7e8d734c0f39
Dorohov <dorohov@lanit.ru>
2016-12-15 17:41:47 +0300

    Bug Fix

--- common/src/main/java/com/technoserv/db/model/configuration/SystemSettingsType.java
+++ common/src/main/java/com/technoserv/db/model/configuration/SystemSettingsType.java
@@ -4,7 +4,7 @@ package com.technoserv.db.model.configuration;
  * Created by 90630 on 14.12.2016.
  */
 public enum SystemSettingsType {
-    PHOTO_PERSIST_SERVICE_URL("http://localhost:8080/storage/rest/image"),
+    PHOTO_PERSIST_SERVICE_URL("http://www.sdorohov.ru/storage/api/rest/image"),
     COMPARE_SERVICE_URL("http://localhost:8080/rpe/api/rest/compare-stub/template"),
     PHOTO_ANALYZER_SERVICE_URL("http://localhost:8080/analize/analize"),
     TEMPLATE_BUILDER_SERVICE_URL("http://localhost:8080/rpe/api/rest/template-builder-stub"),
