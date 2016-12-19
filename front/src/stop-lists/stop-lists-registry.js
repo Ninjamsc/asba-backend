577e073bdd8446ef89edd98b6b636ec8c45ab1cc
vbasakov <vbasakov@gmail.com>
2016-12-19 20:45:34 +0400

    Подгонка реестра стоп-листов под реальные данные

--- front/src/stop-lists/stop-lists-registry.js
+++ front/src/stop-lists/stop-lists-registry.js
@@ -2,9 +2,7 @@ angular.module('stop-lists-registry', ['ui.router','commons'])
     .controller('stopListsRegistryController', function ($scope, $log, $httpService, $state) {
         $scope.stopLists = [];
 
-        $httpService.findStopLists(function (result) {
-            $scope.stopLists = result.data;
-        });
+        
 
         $scope.editStopList = function (stopList) {
             window.open('#/stoplists/edit?stoplistId=' + stopList.id, '');
@@ -15,5 +13,17 @@ angular.module('stop-lists-registry', ['ui.router','commons'])
             console.log('Deleting stop list');
             console.log(!!stopList ? stopList.id : 'StopList not found');
         };
+
+        $scope.getOwnerId = function (stopList) {
+            if (!!stopList && !!stopList.owner && stopList.owner.length > 0){
+                return stopList.owner[0].id;
+            }
+        };
+
+        //Инициализация
+        $httpService.findStopLists(function (result) {
+            $scope.stopLists = result.data;
+        });
+
 
     });
