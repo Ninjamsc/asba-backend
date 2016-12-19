47079cab32dba9ba7b9d89738f15b83575f1ae93
Vadim Basakov <vbasakov@gmail.com>
2016-12-20 02:05:03 +0500

    Редактирование стоплиста

--- front/src/stop-lists/stop-lists-edit.js
+++ front/src/stop-lists/stop-lists-edit.js
@@ -4,17 +4,23 @@ angular.module('stop-lists-edit', ['ui.router', 'commons', 'angularFileUpload'])
 
         $scope.stateListId = $stateParams.stoplistId;
 
+        $scope.refresh = function () {
+            $httpService.getStoplist($scope.stateListId, function (result) {
+                var data = result.data;
+                $log.info(data);
+                $scope.stoplist = data;
+                if ($scope.stoplist.type !== 'bank') {
+                    $scope.stoplist.type = 'common'
+                }
+            });
+        };
 
-        $httpService.getStoplist($scope.stateListId, function (result) {
-            var data = result.data;
-            $log.info(data);
-            $scope.stoplist = data;
-            if ($scope.stoplist.type !== 'bank') {
-                $scope.stoplist.type = 'common'
-            }
-        });
         $scope.uploader = new FileUploader();
         $scope.uploader.url  ='/rpe/api/stop-list/'+$scope.stateListId+'/upload';
+        $scope.uploader.onSuccessItem = function(fileItem, response, status, headers) {
+            $log.info('Success upload', reposponse);
+            $scope.refresh();
+        };
 
         $scope.uploadFile = function () {
             //https://github.com/nervgh/angular-file-upload/wiki/Module-API
@@ -27,6 +33,9 @@ angular.module('stop-lists-edit', ['ui.router', 'commons', 'angularFileUpload'])
             $httpService.editStoplist($scope.stoplist, function(data){
                 $log.info("success save", data);
             });
-        }
+        };
+
+
+        $scope.refresh();
 
     });
