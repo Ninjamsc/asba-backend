d4a00e311df4149ea6bc757e855cfd6a22fb332f
Vadim Basakov <vbasakov@gmail.com>
2016-12-20 02:19:48 +0500

    Редактирование стоплиста

--- front/src/stop-lists/stop-lists-edit.js
+++ front/src/stop-lists/stop-lists-edit.js
@@ -4,20 +4,28 @@ angular.module('stop-lists-edit', ['ui.router', 'commons', 'angularFileUpload'])
 
         $scope.stateListId = $stateParams.stoplistId;
 
+        $log.info($scope.stateListId);
         $scope.refresh = function () {
-            $httpService.getStoplist($scope.stateListId, function (result) {
-                var data = result.data;
-                $log.info(data);
-                $scope.stoplist = data;
-                if ($scope.stoplist.type !== 'bank') {
-                    $scope.stoplist.type = 'common'
-                }
-            });
+            if (!!$scope.stateListId) {
+                $httpService.getStoplist($scope.stateListId, function (result) {
+                    var data = result.data;
+                    if (!!data) {
+                        $scope.stoplist = data;
+                        if ($scope.stoplist.type !== 'bank') {
+                            $scope.stoplist.type = 'common'
+                        }
+                    }
+                    else {
+                        $scope.stoplistNotFound = true;
+                        $log.info('StopList not found');
+                    }
+                });
+            }
         };
 
         $scope.uploader = new FileUploader();
-        $scope.uploader.url  ='/rpe/api/stop-list/'+$scope.stateListId+'/upload';
-        $scope.uploader.onSuccessItem = function(fileItem, response, status, headers) {
+        $scope.uploader.url = '/rpe/api/stop-list/' + $scope.stateListId + '/upload';
+        $scope.uploader.onSuccessItem = function (fileItem, response, status, headers) {
             $log.info('Success upload', response);
             $scope.refresh();
         };
@@ -29,8 +37,8 @@ angular.module('stop-lists-edit', ['ui.router', 'commons', 'angularFileUpload'])
             $scope.uploader.queue[0].upload();
         };
 
-        $scope.saveStoplist = function(){
-            $httpService.editStoplist($scope.stoplist, function(data){
+        $scope.saveStoplist = function () {
+            $httpService.editStoplist($scope.stoplist, function (data) {
                 $log.info("success save", data);
             });
         };
