1ace120366df91d4b51ff0f12de9a6e353b01f10
mlevitin <mlevitin@technoserv.com>
2017-03-22 05:21:42 +0300

    Compare 2 photos service

---
+++ similarity/src/main/java/com/technoserv/rest/model/CompareRequest.java
@@ -0,0 +1,33 @@
+package com.technoserv.rest.model;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class CompareRequest {
+
+
+	@JsonProperty("pictureA")
+	private String pictureA;
+
+	@JsonProperty("pictureB")
+	private String pictureB;
+
+	public String getPictureB() {
+		return pictureB;
+	}
+	public void setPictureB(String pictureB) {
+		this.pictureB = pictureB;
+	}
+	public String getPictureA() {
+		return pictureA;
+	}
+	public void setPictureA(String pictureA) {
+		this.pictureA = pictureA;
+	}
+
+	@Override
+	public String toString() {
+		return "CompareRequest  [pictureA=" + pictureA + ", pictureB="+pictureB+"]";
+	}
+
+}
