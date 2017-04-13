25769ee636fe2aba3cef360fb5311b1889f5e117
Alexander Konovalov <akonovalov@technoserv.com>
2017-04-14 02:09:31 +0300

    implement method getPhotoTemplateFromTevianCore

--- common/src/main/java/com/technoserv/rest/request/PhotoTemplate.java
+++ common/src/main/java/com/technoserv/rest/request/PhotoTemplate.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
  * Created by VBasakov on 22.11.2016.
  */
 public  class PhotoTemplate {
+
     /** версия сети, с помощь. которой построен шаблон */
     public int version;
     /** массив Numeric	биометрический шаблон */
@@ -19,14 +20,26 @@ public  class PhotoTemplate {
 
     /**Тип свёртки*/
     public int type;
-    
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    public void setTemplate(double[] template) {
+        this.template = template;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
     @Override
     public String toString()
     {
     	Double[] arr =  new Double[template.length];
     	for(int i=0;i< template.length;i++)
     		arr[i] = new Double(template[i]);
-    	return new ArrayList<Double>(Arrays.asList(arr)).toString();
+    	return new ArrayList<Double>(Arrays.asList(arr)).toString() + "; version = " + version + "; type = " + type;
     }
 
 }
