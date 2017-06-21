88b09e9296f12883114ed3ef2fec70b19dd7b341
serega2rikov <serega2rikov@me.com>
2017-06-21 18:33:04 +0300

    List of all pictures

--- request-processing-engine-web/src/main/java/com/technoserv/rest/resource/CompareResultResource.java
+++ request-processing-engine-web/src/main/java/com/technoserv/rest/resource/CompareResultResource.java
@@ -7,8 +7,11 @@ import com.technoserv.db.model.objectmodel.CompareResult;
 import com.technoserv.db.model.objectmodel.Document;
 import com.technoserv.db.model.objectmodel.Request;
 import com.technoserv.db.service.objectmodel.api.CompareResultService;
+import com.technoserv.db.service.objectmodel.api.DocumentService;
 import com.technoserv.db.service.objectmodel.api.RequestService;
 import com.technoserv.rest.model.CompareReport;
+import com.technoserv.rest.model.CompareResponseDossierReport;
+import com.technoserv.rest.model.CompareResponsePhotoObject;
 import com.technoserv.utils.HttpUtils;
 import io.swagger.annotations.Api;
 import org.slf4j.Logger;
@@ -20,7 +23,7 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import java.io.IOException;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by sergey on 27.11.2016.
@@ -38,7 +41,62 @@ public class CompareResultResource {
     @Autowired
     private RequestService requestService;
 
+    @Autowired
+    private DocumentService documentService;
+
     private final ObjectMapper objectMapper = new ObjectMapper();
+
+    void addToList(Collection list, String value){
+
+        if (value != null){
+            int lastSlashPosition = value.lastIndexOf("/");
+            list.add(value.substring(lastSlashPosition+1,value.length()));
+        }
+
+    }
+
+    @GET
+    @Produces(HttpUtils.APPLICATION_JSON_UTF8)
+    @Consumes(HttpUtils.APPLICATION_JSON_UTF8)
+    @JacksonFeatures(serializationEnable = {SerializationFeature.INDENT_OUTPUT})
+    @Path("/list") //id as wfm
+    public Response findAll() throws IOException {
+        List<CompareResult> compareResults = compareResultService.getAll();
+        List<Request> req = requestService.getAll();
+        List<CompareReport> compareReports = new ArrayList<>(compareResults.size());
+        List<Document> documents = documentService.getAll();
+
+        Set<String> finalResult = new TreeSet<>();
+
+        for (CompareResult compareResult : compareResults) {
+            CompareReport report = objectMapper.readValue(compareResult.getJson(), CompareReport.class);
+            addToList(finalResult,report.getScannedPicture().getPictureURL()); //finalResult.add(report.getScannedPicture().getPictureURL());
+            addToList(finalResult,report.getScannedPicture().getPreviewURL()); //finalResult.add(report.getScannedPicture().getPreviewURL());
+            addToList(finalResult,report.getCameraPicture().getPictureURL()); //finalResult.add(report.getCameraPicture().getPictureURL());
+            addToList(finalResult,report.getCameraPicture().getPreviewURL()); //finalResult.add(report.getCameraPicture().getPreviewURL());
+            addToList(finalResult,report.getScannedPicturePreviewURL()); //finalResult.add(report.getScannedPicturePreviewURL());
+            addToList(finalResult,report.getWebCamPicturePreviewURL()); //finalResult.add(report.getWebCamPicturePreviewURL());
+            addToList(finalResult,report.getScannedPictureURL()); //finalResult.add(report.getScannedPictureURL());
+            addToList(finalResult,report.getWebCamPictureURL()); //finalResult.add(report.getWebCamPictureURL());
+            if(report.getOthernessPictures() != null) for (CompareResponsePhotoObject po : report.getOthernessPictures().getPhotos()){
+                addToList(finalResult,po.getUrl()); //finalResult.add(po.getUrl());
+            }
+            if(report.getSimilarPictures() != null) for (CompareResponsePhotoObject po : report.getSimilarPictures().getPhotos()){
+                addToList(finalResult,po.getUrl()); //finalResult.add(po.getUrl());
+            }
+        }
+
+        for (Document d : documents) {
+            addToList(finalResult,d.getFaceSquare()); //finalResult.add(d.getFaceSquare());
+            addToList(finalResult,d.getOrigImageURL()); //finalResult.add(d.getOrigImageURL());
+        }
+
+ //       finalResult.add(String.valueOf(finalResult.size()));
+
+        return (finalResult != null)
+                ? Response.ok(finalResult).build()
+                : Response.status(Status.NOT_FOUND).build();
+    }
 
     @GET
     @Produces(HttpUtils.APPLICATION_JSON_UTF8)
